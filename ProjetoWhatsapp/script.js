064ebef421e4349097be1f778469398fa00046f5
Henrique Eidt <henriqueidt@gmail.com>
2018-09-28 18:41:47 -0300

    menu de troca de abas criado

--- ProjetoWhatsapp/script.js
+++ ProjetoWhatsapp/script.js
@@ -2,41 +2,63 @@ const object = {
     name: "Thiago",
     photo: '_images/obama.jpg',
     lastMessage: 'Lorem ipsum ante habitant',
-    time: '20:33'
+    time: '20:33',
+    phone: '+55 51 99723-5837'
 }
 
 const object2 = {
     name: "Trump",
     photo: 'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg',
     lastMessage: 'Hello my friend!!',
-    time: '23:33'
+    time: '23:33',
+    phone: '+55 51 99383-1234'
 }
 
 const object3 = {
     name: "Bill Clinton",
     photo: 'https://pbs.twimg.com/profile_images/451207149478096896/HoMUOmyu_400x400.jpeg',
     lastMessage: 'Heyoo!! whanna tc?',
-    time: '13:20'
+    time: '13:20',
+    phone: '+55 51 99283-2953'
 }
 
 const object4 = {
     name: "Dilma",
     photo: 'https://pbs.twimg.com/profile_images/1030086754441879552/rwdaiqVE_400x400.jpg',
     lastMessage: 'Oi. Quer estocar vento?',
-    time: '13:20'
+    time: '13:20',
+    phone: '+55 51 99324-2344'
 }
 
 const object5 = {
     name: "Temer",
     photo: 'https://pbs.twimg.com/profile_images/1015255524269535232/sSICnYpw_400x400.jpg',
     lastMessage: 'Oi é o dracula',
-    time: '13:20'
+    time: '13:20',
+    phone: '+55 51 99213-1245'
 }
 
 const users = [object, object2, object3, object4, object5, object, object2, object3, object4, object5, object, object2, object3, object4, object5]
 
-const htmlFinal = users.map(function(user) {
-    console.log(user)
+const telephones = [object, object2, object3, object4, object5]
+
+const numbers = telephones.map(function(telephone) {
+    return `
+    <li>
+        <img class="contact-photo" src=${telephone.photo}>
+        <section class="information">
+            <div class="list-name-message">
+                <div class="name-time"><p>${telephone.name}</p><div class="time"></div></div>
+                <p class="list-message">${telephone.phone}</p>
+            </div>
+        </section>
+    </li> 
+    `
+})
+
+//document.querySelector('.contacts').innerHTML = htmlFInal2.join("")
+
+const conversations = users.map(function(user) {
     return ` 
     <li>
         <img class="contact-photo" src=${user.photo}>
@@ -50,39 +72,32 @@ const htmlFinal = users.map(function(user) {
 `
 })
 
-document.querySelector('.contacts').innerHTML = htmlFinal.join("")
+document.querySelector('.contacts').innerHTML = conversations.join("")
 
-const searchContact = document.forms['conversations-search-bar'].querySelector('.search-contact');
+document.querySelector('.tab1-button').addEventListener('click', function(event) {
+    document.querySelector('.contacts').innerHTML = numbers.join("")
+    document.querySelector('.tab1-button').style="background: #a2c3c3"
+    document.querySelector('.tab2-button').style="background: #e0ebeb"
+})
 
-function myFunction(){
-    var i, input, ul, li, input;
-    input = document.querySelector('.search-contact');
-    filter = input.value.toUpperCase();
-    ul = document.querySelector('.contacts');
-    li = ul.getElementsByTagName('li');
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName('p')[0];
-        if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+document.querySelector('.tab2-button').addEventListener('click', function(event) {
+    document.querySelector('.contacts').innerHTML = conversations.join("")
+    document.querySelector('.tab2-button').style="background: #a2c3c3"
+    document.querySelector('.tab1-button').style="background: #e0ebeb"
+})
 
 document.querySelector('.search-contact').addEventListener('keyup', function(e) {
-    var a, b;
+    var name, text, i, input;
     ul = document.querySelector('.contacts');
     li = ul.getElementsByTagName('li');
     input = document.querySelector('.search-contact').value.toLowerCase();
     for (i = 0; i < li.length; i++) {
-        a = li[i].querySelector('.name-time').querySelector('p').innerHTML.toLowerCase();
-        b = li[i].querySelector('.list-message').innerHTML.toLowerCase();
-        if(a.indexOf(input) > -1 || b.indexOf(input) > -1){
+        name = li[i].querySelector('.name-time').querySelector('p').innerHTML.toLowerCase();
+        text = li[i].querySelector('.list-message').innerHTML.toLowerCase();
+        if(name.indexOf(input) > -1 || text.indexOf(input) > -1){
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         }
     }
-
 });
